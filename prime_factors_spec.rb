a055c2970a8647203ab80a3459e8d0a6b410944c
anaganaga <nitinrajkumar502@gmail.com>
2025-05-12 11:16:19 +0530

    test: specs for n=6,8,18,25,64,74

--- prime_factors_spec.rb
+++ prime_factors_spec.rb
@@ -21,4 +21,32 @@ RSpec.describe "Prime Factors" do
 	it "returns [2,2] when input is 4" do
 		expect(prime_factors(4)).to eq([2, 2])
 	end
+
+	it "returns [2,3] when input is 6" do
+		expect(prime_factors(6)).to eq([2, 3])
+	end
+
+	it "returns [2,2,2] when input is 8" do
+		expect(prime_factors(8)).to eq([2, 2, 2])
+	end
+
+	it "returns [3,3] when input is 9" do
+		expect(prime_factors(9)).to eq([3, 3])
+	end
+
+	it "returns [2,3,3] when input is 18" do
+		expect(prime_factors(18)).to eq([2, 3, 3])
+	end
+
+	it "returns [5, 5] when input is 25" do
+		expect(prime_factors(25)).to eq([5, 5])
+	end
+
+	it "returns [2, 2, 2, 2, 2, 2] when input is 64" do
+		expect(prime_factors(64)).to eq([2, 2, 2, 2, 2, 2])
+	end
+
+	it "returns [2, 37] when input is 74" do
+		expect(prime_factors(74)).to eq([2, 37])
+	end
 end
